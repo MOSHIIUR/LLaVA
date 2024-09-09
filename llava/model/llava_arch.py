4adb7002ebe6fa2518d41db00c32af1e4d1254a4
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-09-09 07:50:55 +0600

    fixing bug in cross attn

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -667,7 +667,6 @@ class LlavaMetaForCausalLM(ABC):
                 # print(cur_input_embeds_no_im.shape)
                 cur_labels_noim = text_labels[x]
                 split_sizes = splits[x]
-                # print(f"split_sizes: {split_sizes}, type: {type(split_sizes)}")
 
                 cur_input_embeds_no_im = torch.split(cur_input_embeds_no_im, split_sizes, dim=0)
                 
@@ -675,6 +674,7 @@ class LlavaMetaForCausalLM(ABC):
                     # Handle the case where split_sizes is string, e.g., skip splitting
                     cur_input_embeds_no_im = cur_input_embeds_no_im
                 else:
+                    print(f"split_sizes: {split_sizes}, type: {type(split_sizes)}")
                     cur_input_embeds_no_im = torch.split(cur_input_embeds_no_im, split_sizes, dim=0)
 
                 cur_new_input_embeds = []
