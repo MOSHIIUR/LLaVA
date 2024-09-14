4404f12df41bdeaa0530b799bc86a8111b4280f3
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-09-14 14:35:08 +0600

    debugging

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -549,7 +549,7 @@ class LlavaMetaForCausalLM(ABC):
                 new_input_embeds.append(cur_input_embeds)
                 new_labels.append(labels[batch_idx])
                 cur_image_idx += 1
-                print(f'image embed: {img_embed.shape}')
+                # print(f'image embed: {img_embed.shape}')
                 continue
             # cur_input_ids = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])  
             # labels = torch.tensor([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32])  
