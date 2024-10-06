349854142293841d5361658ac6483d8931eef51a
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-10-07 01:15:39 +0600

    eval phi2 using VLMEvalKit

--- llava/eval/model_vqa.py
+++ llava/eval/model_vqa.py
@@ -44,6 +44,7 @@ def eval_model(args):
         image_file = line["image"]
         qs = line["text"]
         cur_prompt = qs
+        
         if model.config.mm_use_im_start_end:
             qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
         else:
