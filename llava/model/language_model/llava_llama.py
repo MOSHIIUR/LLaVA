bd4401e4ac45c21ca076f830288e638127cdf4ab
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-10-06 19:47:45 +0600

    pop cache position if exist

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -221,7 +221,10 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM,):
         inputs = super().prepare_inputs_for_generation(
             input_ids, past_key_values=past_key_values, inputs_embeds=inputs_embeds, **kwargs
         )
-        inputs.pop("cache_position")
+        
+        # Pop the key 'cache_position' if it exists
+        inputs.pop("cache_position", None)
+        
         if images is not None:
             inputs['images'] = images
         if image_sizes is not None:
