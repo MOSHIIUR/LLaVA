0792d901494b72e36511e5999939b43d58ed63a9
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-10-19 19:09:52 +0600

    debugging concatanated tokens

--- llava/model/language_model/utils/utils.py
+++ llava/model/language_model/utils/utils.py
@@ -1,17 +1,21 @@
 import torch
 
-def split_seqeunce(text_split, img_seq, input_embeds):
+def split_seqeunce(text_splits, img_sequences, input_embeds):
     text_hidden_states, img_hidden_states = [], []
-    for idx, input_embed in enumerate(input_embeds):
-        txt_seq_len = sum(text_split[idx])
-        input_seq_len = input_embeds[idx].shape[0]
-        split_size = [txt_seq_len, img_seq[idx], input_seq_len]
+    for text_split, img_sequence, input_embed in zip(text_splits, img_sequences, input_embeds):
+        txt_seq_len = sum(text_split)
+        total_seq_len = input_embed.shape[0]
+        split_size = [txt_seq_len, img_sequence, total_seq_len - (txt_seq_len + txt_seq_len)]
+        
         print(f'split size: {split_size}')
+
         text_tokens, img_tokens, padded_tokens = torch.split(input_embed, split_size, dim=0)
         text_tokens = torch.cat((text_tokens, padded_tokens), dim=0)
         text_hidden_states.append(text_tokens)
-        if img_seq == 0:
+        
+        if img_sequence == 0:
             print(f'image hidden feature: {img_tokens.shape}')
+        
         img_hidden_states.append(img_tokens)
 
     new_text_hidden_states = torch.stack(text_hidden_states, dim=0)
