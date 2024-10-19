df765f3bc3af1923b2c05ec8fb3e7eebabd85fa1
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-10-19 12:35:37 +0600

    debug llm moe modules

--- llava/model/language_model/llava_llama_moe.py
+++ llava/model/language_model/llava_llama_moe.py
@@ -481,13 +481,17 @@ class MoELLaVALlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
 
     def initialize_moe_modules(self, model_args):
 
+        rank0_print('initializing moe modules')
 
         self.config.num_experts = model_args.num_experts
         self.config.num_experts_per_tok = model_args.num_experts_per_tok
         self.config.router_aux_loss_coef = model_args.router_aux_loss_coef
         num_layers = self.config.num_hidden_layers
 
+        rank0_print(f'num_layers: {num_layers}')
+
         for layer_idx in range(num_layers):
+            rank0_print(f'layer idx: {layer_idx}')
             pretrained_state_dict = self.model.layers[layer_idx].mlp.state_dict()
             llama_mlp = self.model.layers[layer_idx].mlp
             self.model.layers[layer_idx].mlp = LlamaSparseMoeBlock(self.config, llama_mlp)
